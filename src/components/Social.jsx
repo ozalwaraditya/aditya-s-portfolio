36228b11f99bc860cd5a01585bd229051c72e3e9
ozalwaraditya <adityaozalwar03@gmail.com>
2025-02-28 01:13:16 +0530

    Added Home Component

---
+++ src/components/Social.jsx
@@ -0,0 +1,33 @@
+const Social = () => {
+    return (
+      <div className="home__social">
+        <a
+          href="https://www.instagram.com/adityaozalwar"
+          className="home__social-icon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="uil uil-instagram"></i>
+        </a>
+  
+        <a
+          href="https://www.github.com/ozalwaraditya"
+          className="home__social-icon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="uil uil-github"></i>
+        </a>
+  
+        <a
+          href="mailto:adityaozalwar14@gmail.com?subject=Hello%20Aditya&body=Hi%20there!"
+          className="home__social-icon"
+        >
+          <i className="uil uil-envelope"></i>
+        </a>
+      </div>
+    );
+  };
+  
+  export default Social;
+  
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import Social from "../components/Social"
+import Data from "../components/Data"
+import '../components/style/Home.css'
+import ScrollDown from "../components/ScrollDown"
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social/>
+
+          <div className="home__img"></div>
+
+          <Data/>
+        </div>
+
+        <ScrollDown/>
+      </div>
+    </section>
+  )
+}
+
+export default Home
